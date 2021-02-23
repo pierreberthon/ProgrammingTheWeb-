dfd859c276021919a91e3b57e1aa8e347081b146
Glitch (part-1-programming-the-web) <none>
2021-02-23 11:49:01 +0000

    🌔🍜 Updated with Glitch

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ $(".button").on('click', function() {
   const tvShows = []
   $.getJSON('https://api.tvmaze.com/search/shows?q=silicon-valley', function(myDataset) {
     myDataset.forEach((element) => {
-      console.log(element.show);
       tvShows.push(element);
+      $('body').append("<p>" + element.show.runtime + "</p>");
+      $('body').append("<p>" + element.show.name + "</p>");
+      $('body').append("<p>" + element.show.summary + "</p>");
     });
   });
 });
